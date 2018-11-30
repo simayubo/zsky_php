f97dce44e517d7966b84df74a1aff8494244096e
simayubo@gmail.com <simayubo123>
2018-11-30 22:27:10 +0800

    初始化

--- application/common/controller/Admin.php
+++ application/common/controller/Admin.php
@@ -11,6 +11,7 @@ class Admin extends Common {
     public function __construct()
     {
         parent::__construct();
+        exit('小子，你想干啥？');
         self::$admin = \app\common\model\Admin::get(Session::get('admin')['id']);
     }
 
