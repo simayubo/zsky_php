88909f5089fd99bf3d753fa8f8c808e260797721
haoxq <simayubo@gmail.com>
2018-11-30 16:55:54 +0800

    初始化

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -43,13 +43,56 @@ class Index extends Common
     /**
      * 搜索结果
      * @param $keyword
+     * @param string $type
      * @param int $page
      * @return \think\response\View
+     * @throws \ErrorException
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
      */
     public function searchResult($keyword, $type = '', $page = 1){
 
+        $result = ['total' => 0, 'sec' => 0, 'error' => '', 'warning' => '', 'list' => []];
+        if (!empty($keyword)){
+            $sphinx = new SphinxClient();
+            $sphinx->setServer('185.246.85.49', 9312);
+            $sphinx->setSortMode(2, 'requests');
+            $sphinx->setLimits(0,20,50000);
+            $ret = $sphinx->query($keyword);
 
+            if (empty($ret)){
+                $result['list'] = [];
+            }else{
+                $result['total'] = $ret['total_found'];
+                $result['sec'] = $ret['time'] * 1000;
+                $result['error'] = $ret['error'];
+                $result['warning'] = $ret['warning'];
 
+                //查询ids组合
+                $hash_ids = [];
+                foreach ($ret['matches'] as $key => $item) {
+                    $hash_ids[] = $item['attrs']['info_hash'];
+                }
+                //查询关联文件表
+                $files = Db::name('search_filelist')->whereIn('info_hash', $hash_ids)->select();
+                $files_list = [];
+                foreach ($files as $item) {
+                    $files_list[$item['info_hash']] = $item['file_list'];
+                }
+                //拼装数组列表
+                $result_list = [];
+                foreach ($ret['matches'] as $item) {
+                    $_files = isset($files_list[$item['attrs']['info_hash']])?$files_list[$item['attrs']['info_hash']]:'[]';
+                    $result_list[] = array_merge($item['attrs'], [
+                        'files' => json_decode($_files, true)
+                    ]);
+                }
+                $result['list'] = $result_list;
+            }
+
+        }
+        dump($result);exit();
 
         return view('list')->assign([
             'keyword' => $keyword,
@@ -86,12 +129,12 @@ class Index extends Common
                 'today' => $cache_count['today'],
             ];
         }else{
-            $total = Db::name('search_hash')->field('ifnull(max(id),0)-ifnull(min(id),0)+1 as rows')->find();
+            $total = Db::name('search_hash')->field('ifnull(max(id),0)-ifnull(min(id),0) as rows')->find();
             $today = Db::name('search_hash')->where('create_time', '>', date('Y-m-d'))->count('id');
-            Cache::set('index_count', ['total' => $total, 'today' => $today], 3000);
+            Cache::set('index_count', ['total' => $total['rows'], 'today' => $today], 3000);
 
             return [
-                'total' => $total,
+                'total' => $total['rows'],
                 'today' => $today,
             ];
         }
