77e1db6bee1024b8aceab92af81565e2c094ebee
simayubo@gmail.com <simayubo123>
2018-11-30 20:49:37 +0800

    初始化

--- config/zsky.php
+++ config/zsky.php
@@ -10,6 +10,7 @@ return [
     'SITE_DESCRIPTION' => Env::get('SITE_DESCRIPTION', '网站介绍'),
     'SITE_HOME_WELCOME' => Env::get('SITE_HOME_WELCOME', '网站首页横幅文字'),
     'SITE_LIKE_TAGS' => Env::get('SITE_LIKE_TAGS', ''),
+    'SITE_LIST_RIGHT_TEST' => Env::get('SITE_LIST_RIGHT_TEST', '电影磁力链接,电影bt种子下载,电影迅雷下载相关资源由用户搜索指令自动获取，结果来源于网络，本站不存储任何资料，亦不承担任何责任，请于24小时内删除！'),
 
     'SITE_FOOTER_HTML' => '<span><a href="#">纸上烤鱼</a></span> | <span><a href="#">种子搜索</a></span> | <span><a href="#">磁力链接</a></span>',
     'SITE_FOOTER_JS' => '<script charset="gbk" type="text/javascript" src="//www.baidu.com/js/opensug.js"></script><script type="text/javascript">var params = { "XOffset": 0, "YOffset": 0, "fontColor": "#444", "fontColorHI": "#000", "fontSize": "16px", "fontFamily": "arial", "borderColor": "gray", "bgcolorHI": "#ebebeb", "sugSubmit": false };BaiduSuggestion.bind("search", params);</script>',
